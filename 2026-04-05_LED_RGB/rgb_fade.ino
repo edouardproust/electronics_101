1c22243627d30fc9e0ffdcb2bc5a89eec95380a1
Edouard Proust <contact@edouardproust.dev>
2026-04-06 01:47:17 +0200

    day 1 code comments

--- 2026-04-05_LED_RGB/rgb_fade.ino
+++ 2026-04-05_LED_RGB/rgb_fade.ino
@@ -1,10 +1,12 @@
-// define pins
 const int n = 3;		   // number of colors (R, G, B)
 int pins[n] = {2, 4, 5};   // GPIO pins for R, G, B respectively
 int vals[n] = {255, 0, 0}; // 255 means full brightness, 0 means off
 int delay_ms = 10;		   // delay in milliseconds between each step of the fade
 int val_increment = 1;	   // how much to increase/decrease the brightness of each color in each step of the fade
 
+/**
+ * setup function runs once when the microcontroller is powered on or reset
+ */
 void setup()
 {
 	for (int i = 0; i < n; ++i)
@@ -13,11 +15,14 @@ void setup()
 	}
 }
 
-// 'from' is the index of the current color that is fading out
-// 'to' is the index of the next color that is fading in
+// 'from' is the index of the current color that is fading out. 'to' is the index of the next color that is fading in
 int from = 0;
 int to = 1;
 
+/**
+ * loop function runs repeatedly after setup() is called.
+ * It runs as fast as possible, so we always use a delay to control the frequency of the loop and avoid overwhelming the microcontroller.
+ */
 void loop()
 {
 	// fade from one color to the next
